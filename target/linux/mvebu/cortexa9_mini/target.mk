4316a360e1318df9d1f680e49cf0c9a50d2fc2bc
Jeff Collins <jeffcollins9292@gmail.com>
2020-08-08 14:06:08 -0400

    mvebu: new subtarget
    Signed-off-by: Jeff Collins <jeffcollins9292@gmail.com>

---
+++ target/linux/mvebu/cortexa9_mini/target.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2017 Hauke Mehrtens
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+include $(TOPDIR)/rules.mk
+
+ARCH:=arm
+BOARDNAME:=Marvell Armada (mini wrt1900ac-v1 wrt32x)
+CPU_TYPE:=cortex-a9
+CPU_SUBTYPE:=neon
+KERNELNAME:=zImage dtbs
